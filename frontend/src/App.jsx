f7d286530ea38c27b79d03a344854419ff34105b
Santiago Javier Rubio <s.rubio91@gmail.com>
2025-05-24 15:47:38 -0300

    refactor merge

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,8 @@ import {Box, useColorModeValue} from "@chakra-ui/react";
 import Navbar  from "./components/Navbar";
 import  HomePage  from "./pages/HomePage";
 import CreatePage from "./pages/CreatePage";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import ProtectedRoute from "./components/authentication/ProtectedRoute";
-import Products from "./pages/Products";
 import Login from "./pages/Login";
 
 function App() {
@@ -14,10 +13,17 @@ function App() {
       <Box minH={"100vh"}bg={useColorModeValue("gray.100","gray.900")}>
         <Navbar/>
         <Routes>
-          <Route path="/" element={<HomePage/>} />
-          <Route path="/create" element={<CreatePage/>} />
-          <Route path="/Productos" element={<ProtectedRoute><Products /></ProtectedRoute>} />
-          <Route path="/Ingreso" element={<Login />} />
+          <Route path="/" element={
+            <ProtectedRoute>
+              <HomePage/>
+            </ProtectedRoute>
+          } />
+          <Route path="/create" element={
+            <ProtectedRoute>
+              <CreatePage/>
+              </ProtectedRoute>
+            } />
+          <Route path="/ingreso" element={<Login />} />
         </Routes>
 
       </Box>
