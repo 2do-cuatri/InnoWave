274a59ed60838d1d895fe2b3ec59c5646c24fd73
Santiago Javier Rubio <s.rubio91@gmail.com>
2025-05-24 16:45:41 -0300

    backend: agrego autenticacion a las rutas de productos

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,23 +1,24 @@
 import express from "express";
 
 import { createProduct, deleteProduct, getProducts, updateProduct, updateProductStock } from "../controllers/product.controller.js";
+import { authenticate } from "../auth/authenticate.js";
 
 
 const router = express.Router();
 
 //Obtener todos los productos
-router.get("/", getProducts)
+router.get("/", authenticate, getProducts)
 
 //Crear un producto
-router.post("/", createProduct)
+router.post("/", authenticate, createProduct)
 
 //Actualizar un producto
-router.put("/:id", updateProduct)
+router.put("/:id", authenticate, updateProduct)
 
 //Actualizar el stock de un producto
-router.patch("/:id", updateProductStock)
+router.patch("/:id", authenticate, updateProductStock)
 
 //Eliminar un producto
-router.delete("/:id", deleteProduct)
+router.delete("/:id", authenticate, deleteProduct)
 
 export default router;
