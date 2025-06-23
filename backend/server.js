bb939e5d9db6144f9db1d1eb0e876ac8a2d365b8
Santiago Javier Rubio <s.rubio91@gmail.com>
2025-06-23 18:05:49 -0300

    refactor: servir dist del FE desde el back

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,9 @@ import { connectDB } from './config/db.js';
 import cookieParser from 'cookie-parser';
 import { errorHandler } from './errors/errorHandler.js';
 import cors from 'cors'
+import path from 'path'
+import { fileURLToPath } from 'url';
+
 
 import productRoutes from "./routes/product.route.js";
 import authRoutes from "./routes/auth.route.js";
@@ -11,6 +14,8 @@ import reportRoutes from "./routes/report.route.js";
 
 
 dotenv.config();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -23,6 +28,9 @@ app.use(cors({
 app.use(express.json()); //Permite recibir datos en formato JSON
 app.use(cookieParser());
 
+// Servir estaticos del FE
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
 // Rutas
 app.use("/api/products", productRoutes);
 app.use("/api/auth", authRoutes);
@@ -31,6 +39,11 @@ app.use("/api/reports", reportRoutes);
 
 // Es importante que este sea el ultimo middleware en la lista
 app.use(errorHandler)
+
+// Todas las demas rutas no indicadas en la API seran dirigidas al FE
+app.get('/*path', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+});
 
 // Iniciar servidor
 app.listen(PORT, () => {
